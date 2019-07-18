f393663e97a23fac4f4682e32414bf85363c4117
gf-espedia <giulia.favaro@espedia.it>
2019-07-18 13:51:23 +0000

    Update in order to work in any portal

--- webapp/controller/TileKPIDashboardView1.controller.js
+++ webapp/controller/TileKPIDashboardView1.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
 	return Controller.extend("TileKPIDashboard.TileKPIDashboard.controller.TileKPIDashboardView1", {
 		onInit: function () {
-			window.location.replace("https://flpnwc-m9a44f3468.dispatcher.hana.ondemand.com/sites/espediasimplemaintenance#Shell-home ");
+			history.back();
 			window.open("https://kpidashboard-m9a44f3468.dispatcher.hana.ondemand.com", '_blank');
 		}
 	});
